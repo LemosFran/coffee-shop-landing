f8c91443dbd0132fc6396dbfd2357a69482ab7e5
Franco <you@example.com>
2024-01-31 19:41:57 -0300

    feat:responsive design

---
+++ portfolio-coffee-shop-files/script.js
@@ -0,0 +1,8 @@
+function toggleMenu()
+{
+    var menu = document.querySelector(".menuToggle");
+    var nav = document.querySelector(".navBar");
+    menu.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+
